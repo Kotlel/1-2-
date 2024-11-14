7e592d2fe21ad9ce956af4efbf7ac79da809546c
Kotlel <kashtanovradmir@gmail.com>
2024-11-14 23:54:27 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // Task 1
+             Console.WriteLine("MinByte {0}", Byte.MinValue);
+             Console.WriteLine("MinInt16 {0}", Int16.MinValue);
+             Console.WriteLine("MinChar {0}", Char.MinValue);
+             Console.WriteLine("MinDouble {0}", Double.MinValue);
+             Console.WriteLine("MinDecimal {0}", Decimal.MinValue);
+
+             
+             Console.WriteLine("MaxByte {0}", Byte.MaxValue);
+             Console.WriteLine("MaxInt16 {0}", Int16.MaxValue);
+             Console.WriteLine("MaxChar {0}", Char.MaxValue);
+             Console.WriteLine("MaxDouble {0}", Double.MaxValue);
+             Console.WriteLine("MaxDecimal {0}", Decimal.MaxValue);
+
+        }
+    }
+}
---
+++ ConsoleApp2/lab/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    struct account
+    {
+        public int number;
+        public string type;
+        public int balance;
+        public void Print()
+        {
+            Console.WriteLine($"Номер - {number}, Тип - {type}, Баланс - {balance}");
+        }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            BankAccountType cur = BankAccountType.Current;
+            Console.WriteLine(cur);
+        }
+            enum BankAccountType
+            {
+                Current = 10,
+                Savings = 12
+            }
+            // Task 2
+            account acc1 = new account {number = 337, type = "Current", balance = 18000 };
+            acc1.Print();
+            // Task 3
+
+            }
+    }
+}
